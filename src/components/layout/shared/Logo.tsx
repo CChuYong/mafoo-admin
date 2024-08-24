0ccee1df45c6c65f436c40d1ca38d4f7162bbccf
ChuYong <jakgon@naver.com>
2024-08-25 02:44:03 +0900

    fix: fix updates

--- src/components/layout/shared/Logo.tsx
+++ src/components/layout/shared/Logo.tsx
@@ -1,33 +1,9 @@
 'use client'
 
-// React Imports
-import type { CSSProperties } from 'react'
-
-// Third-party Imports
-import styled from '@emotion/styled'
-
-// Component Imports
-import MaterioLogo from '@core/svg/Logo'
-
-// Config Imports
-import themeConfig from '@configs/themeConfig'
 import Image from "next/image";
 
-type LogoTextProps = {
-  color?: CSSProperties['color']
-}
 
-const LogoText = styled.span<LogoTextProps>`
-  color: ${({ color }) => color ?? 'var(--mui-palette-text-primary)'};
-  font-size: 1.25rem;
-  line-height: 1.2;
-  font-weight: 600;
-  letter-spacing: 0.15px;
-  text-transform: uppercase;
-  margin-inline-start: 10px;
-`
-
-const Logo = ({ color }: { color?: CSSProperties['color'] }) => {
+const Logo = () => {
   return (
     <div className='flex items-center min-bs-[24px]'>
       {/*<MaterioLogo className='text-[22px] text-primary' />*/}
