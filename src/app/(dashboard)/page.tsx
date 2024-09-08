4de1d646d7bb9bfca8e9b8344e1a6ce323a13033
송영민 <jakgon@naver.com>
2024-09-08 21:14:20 +0900

    feat: upadte missing pages

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -2,6 +2,8 @@
 import Grid from '@mui/material/Grid'
 
 // Components Imports
+import Typography from '@mui/material/Typography'
+
 import Award from '@views/dashboard/Award'
 import Transactions from '@views/dashboard/Transactions'
 import WeeklyOverview from '@views/dashboard/WeeklyOverview'
@@ -14,62 +16,65 @@ import CardStatVertical from '@components/card-statistics/Vertical'
 import Table from '@views/dashboard/Table'
 
 const DashboardAnalytics = () => {
+  // return (
+  //   <Grid container spacing={6}>
+  //     <Grid item xs={12} md={4}>
+  //       <Award />
+  //     </Grid>
+  //     <Grid item xs={12} md={8} lg={8}>
+  //       <Transactions />
+  //     </Grid>
+  //     <Grid item xs={12} md={6} lg={4}>
+  //       <WeeklyOverview />
+  //     </Grid>
+  //     <Grid item xs={12} md={6} lg={4}>
+  //       <TotalEarning />
+  //     </Grid>
+  //     <Grid item xs={12} md={6} lg={4}>
+  //       <Grid container spacing={6}>
+  //         <Grid item xs={12} sm={6}>
+  //           <LineChart />
+  //         </Grid>
+  //         <Grid item xs={12} sm={6}>
+  //           <CardStatVertical
+  //             title='Total Profit'
+  //             stats='$25.6k'
+  //             avatarIcon='ri-pie-chart-2-line'
+  //             avatarColor='secondary'
+  //             subtitle='Weekly Profit'
+  //             trendNumber='42%'
+  //             trend='positive'
+  //           />
+  //         </Grid>
+  //         <Grid item xs={12} sm={6}>
+  //           <CardStatVertical
+  //             stats='862'
+  //             trend='negative'
+  //             trendNumber='18%'
+  //             title='New Project'
+  //             subtitle='Yearly Project'
+  //             avatarColor='primary'
+  //             avatarIcon='ri-file-word-2-line'
+  //           />
+  //         </Grid>
+  //         <Grid item xs={12} sm={6}>
+  //           <DistributedColumnChart />
+  //         </Grid>
+  //       </Grid>
+  //     </Grid>
+  //     <Grid item xs={12} md={6} lg={4}>
+  //       <SalesByCountries />
+  //     </Grid>
+  //     <Grid item xs={12} lg={8}>
+  //       <DepositWithdraw />
+  //     </Grid>
+  //     <Grid item xs={12}>
+  //       <Table />
+  //     </Grid>
+  //   </Grid>
+  // )
   return (
-    <Grid container spacing={6}>
-      <Grid item xs={12} md={4}>
-        <Award />
-      </Grid>
-      <Grid item xs={12} md={8} lg={8}>
-        <Transactions />
-      </Grid>
-      <Grid item xs={12} md={6} lg={4}>
-        <WeeklyOverview />
-      </Grid>
-      <Grid item xs={12} md={6} lg={4}>
-        <TotalEarning />
-      </Grid>
-      <Grid item xs={12} md={6} lg={4}>
-        <Grid container spacing={6}>
-          <Grid item xs={12} sm={6}>
-            <LineChart />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <CardStatVertical
-              title='Total Profit'
-              stats='$25.6k'
-              avatarIcon='ri-pie-chart-2-line'
-              avatarColor='secondary'
-              subtitle='Weekly Profit'
-              trendNumber='42%'
-              trend='positive'
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <CardStatVertical
-              stats='862'
-              trend='negative'
-              trendNumber='18%'
-              title='New Project'
-              subtitle='Yearly Project'
-              avatarColor='primary'
-              avatarIcon='ri-file-word-2-line'
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <DistributedColumnChart />
-          </Grid>
-        </Grid>
-      </Grid>
-      <Grid item xs={12} md={6} lg={4}>
-        <SalesByCountries />
-      </Grid>
-      <Grid item xs={12} lg={8}>
-        <DepositWithdraw />
-      </Grid>
-      <Grid item xs={12}>
-        <Table />
-      </Grid>
-    </Grid>
+    <Typography variant={'h2'}>마푸 어드민에 오신것을 환영해요!</Typography>
   )
 }
 
